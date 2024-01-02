6d7e66b6631bdeb69f13d665e6f2b3d38a4e45c4
Jonathan Simpson <jsimpson@jdsnetwork.com>
2024-01-02 10:37:05 -0500

    Fix fields loading

--- index.php
+++ index.php
@@ -38,48 +38,53 @@
     <script>
         // JavaScript to handle AJAX requests when a table is clicked
         $(document).ready(function () {
-        var selectedTable = ""; // To store the selected table name
-        var selectedFields = []; // To store the selected field names
+            var selectedTable = ""; // To store the selected table name
+            var selectedFields = []; // To store the selected field names
 
-        $(".table-link").click(function () {
-            selectedTable = $(this).data("table");
-            $.ajax({
+            $(".table-link").click(function () {
+                selectedTable = $(this).data("table");
+                loadFields(selectedTable); // Load fields when a table is clicked
+            });
+
+            $("#field-list").on("click", ".field-link", function () {
+                var field = $(this).data("fields");
+                if (!selectedFields.includes(field)) {
+                    selectedFields.push(field);
+                } else {
+                    // Remove the field if it's already selected
+                    selectedFields = selectedFields.filter(function (value) {
+                        return value !== field;
+                    });
+                }
+                updateQueryText(); // Update the query when a field is selected/unselected
+            });
+
+            function loadFields(table) {
+                $.ajax({
                     url: "get-fields.php",
                     method: "POST",
                     data: { table: table },
                     success: function (data) {
                         $("#field-list-items").html(data);
+                        selectedFields = []; // Clear selected fields when a new table is selected
+                        updateQueryText(); // Update the query
                     }
-            updateQueryText(); // Update the query when a table is selected
-        });
-
-        $("#field-list").on("click", ".field-link", function () {
-            var field = $(this).data("fields");
-            if (!selectedFields.includes(field)) {
-                selectedFields.push(field);
-            } else {
-                // Remove the field if it's already selected
-                selectedFields = selectedFields.filter(function (value) {
-                    return value !== field;
                 });
             }
-            updateQueryText(); // Update the query when a field is selected/unselected
+
+            function updateQueryText() {
+                var query = "SELECT ";
+                if (selectedFields.length > 0) {
+                    query += selectedFields.join(", ") + " ";
+                } else {
+                    query += "* ";
+                }
+                if (selectedTable) {
+                    query += "FROM " + selectedTable;
+                }
+                $("#query-text").val(query);
+            }
         });
-
-    function updateQueryText() {
-        var query = "SELECT ";
-        if (selectedFields.length > 0) {
-            query += selectedFields.join(", ") + " ";
-        } else {
-            query += "* ";
-        }
-        if (selectedTable) {
-            query += "FROM " + selectedTable;
-        }
-        $("#query-text").val(query);
-    }
-});
-
     </script>
 </body>
 </html>
